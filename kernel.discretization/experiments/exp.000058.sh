5604d484ee499f4335f483e6a6507b650ad5f030
joseluis.flores <joseluisfloresbarroso@gmail.com>
2017-05-24 19:33:55 +0200

    New results

---
+++ kernel.discretization/experiments/exp.000058.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+HOST=`hostname`
+PC='computer.local'
+if [ $HOST = $PC ];
+then
+	echo "Local Execution: started"
+	cd /Users/JoseLuis/isg-ehu/joseluis.flores/kernel.discretization/src/R/
+	Rscript K3.R ../../experiments/01.Horse.Colic/config/Database.01.08.csv ../../bbdd/01.Horse.Colic/04.Final.SCV/6/ ../../experiments/01.Horse.Colic/results.06/08/	../../experiments/01.Horse.Colic/results.06/08/ 100 
+	cd /Users/JoseLuis/isg-ehu/joseluis.flores/kernel.discretization
+	echo "Local Execution: finished"
+	exit 0
+fi
+echo "Cluster Execution: started"
+qsub cluster.script.000058.sh
+echo "Cluster Execution: finished"
